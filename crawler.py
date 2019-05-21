751e354b7d54c86c6392f616c1ffa79cd49f8a7d
Kazimov <Anar.Kazimov@gov.bc.ca>
2019-05-21 15:37:58 -0700

    added naming conventino for the webpages

--- crawler.py
+++ crawler.py
@@ -5,15 +5,39 @@
 
 from selenium import webdriver
 import ahk
+import time
+import re
+
+# URLs
+website = "https://employmentprogram.gov.bc.ca/Transition/SitePages/Home.aspx"
 
 driver = webdriver.Chrome('chromedriver.exe') #driver for chrome 74 is attached
-driver.get("https://employmentprogram.gov.bc.ca/Transition/SitePages/Home.aspx")
+driver.get(website)
+
+# Getting the webpage name from the url
+url = driver.current_url
+match = re.search(r"\w*\.aspx", url)
+page = match.group(0)
+page = page.replace("aspx", "html")
+print(page)
+
+# Using hot keys to download the website
 
 ahk.start()
 ahk.ready()
 ahk.execute("Send,^s")
 ahk.execute("WinWaitActive, Save As,,2")
 ahk.execute("WinActivate, Save As")
-ahk.execute("Send, Transition\\file.htm")
+save_as = "Send, C:\\Transition\\" + page
+save_as = save_as.encode('utf8')
+print(type(save_as))
+
+
+sent = "Send, C:\\Transition\\" + "Home.html"
+print(type(sent))
+ahk.execute(save_as)
 ahk.execute("Send, {Enter}")
 
+
+
+#\w*\.aspx
